506d0d3a556837e0b10c0c96056577be6b6a7bfa
November Kilo <natasha.i.koroleva@gmail.com>
2023-01-08 18:37:13 -0600

    remove unused

--- RDR2_Notifications/NativesWrapper.cpp
+++
@@ -1,9 +0,0 @@
-#include "NativesWrapper.h"
-
-Hash NativesWrapper::getHash(char* key) {
-	return GAMEPLAY::GET_HASH_KEY(key);
-}
-
-char* NativesWrapper::literalString(char* str) {
-	return GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", str);
-}
